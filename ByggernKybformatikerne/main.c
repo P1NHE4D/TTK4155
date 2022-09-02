6e04f2c1c4b4ac6218fb57af9ce739617d235f5e
Thomas Holene Løkkeborg <thomahl@ntnu.no>
2022-09-02 10:05:05 +0200

    initial commit

---
+++ ByggernKybformatikerne/main.c
@@ -0,0 +1,18 @@
+/*
+ * ByggernKybformatikerne.c
+ *
+ * Created: 02.09.2022 09:57:58
+ * Author : thomahl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
