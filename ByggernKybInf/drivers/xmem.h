492c021d16ba996d64ceaf390b12ae5dfbd9ff68
tholok97 <thomas.lokkeborg@gmail.com>
2022-09-12 12:27:49 +0200

    Thomas homework: fix xmem driver
    ready for last step of exercise 2

--- ByggernKybInf/drivers/xmem.h
+++ ByggernKybInf/drivers/xmem.h
@@ -10,16 +10,23 @@
 
 
 // Start address for the SRAM
-uint16_t SRAM_BASE_ADDRESS = 0x1800;
+extern const uint16_t SRAM_BASE_ADDRESS;
 
 // Taken from Lab Support Data > Misc. Resources > sram_test.c
-uint16_t SRAM_SIZE = 0x800;
+extern const uint16_t SRAM_SIZE;
 
+// initialize xmem. All other functions in this file require xmem_init to be ran first
 void xmem_init(void);
 
+// write the byte 'data' to the 12-bit address 'addr'
 void sram_write(uint8_t data, uint16_t addr);
 
+// read a byte from sram at the 12-bit address 'addr'
 uint8_t sram_read(uint16_t addr);
+
+// Tests the SRAM driver using a modified version of the code provided at
+// Lab Support Data > Misc. Resources > sram_test.c
+void sram_test();
 
 
 #endif /* XMEM_H_ */
